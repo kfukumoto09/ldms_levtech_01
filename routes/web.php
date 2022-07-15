29a0e6fdb67910caecba262d6885e06a3b81f655
kfukumoto09 <kfukumoto@protein.osaka-u.ac.jp>
2022-07-16 00:05:35 +0900

    laravel/breeze was functionalized with clear design.

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectController;
-use App\Http\Controllers\SubjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,10 +18,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('/', [ProjectController::class, 'index']);
-Route::get('/project/{project}', [ProjectController::class, 'home'])
-        ->where('project','[0-9]{0,3}');
-Route::get('/project/1/{subject}', [SubjectController::class, 'home'])
-        ->where('project_id','[0-9]{0,3}')
-        ->where('subject','[0-9]{0,6}');
+require __DIR__.'/auth.php';
+
+
+Route::get('/index', [ProjectController::class, 'index']);
