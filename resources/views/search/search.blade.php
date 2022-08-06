0738c24fa4b3de9277dca494b6599772176788e7
kfukumoto09 <kfukumoto@protein.osaka-u.ac.jp>
2022-08-06 15:32:51 +0900

    temp

---
+++ resources/views/search/search.blade.php
@@ -0,0 +1,27 @@
+<x-app-layout>
+    
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Search lab notes
+        </h2>
+    </x-slot>
+    
+    
+    <!--Body-->
+    
+    <div class="grid grid-cols-3">
+        <div class='mx-16'>
+            <textarea class='textarea w-32 h-2'></textarea>
+            <input class='w-16 border border-gray-200' value='Search'/>
+        </div>
+        
+        <div>
+
+        </div>
+        
+        <div>
+
+        </div>
+    </div>
+
+</x-app-layout>
