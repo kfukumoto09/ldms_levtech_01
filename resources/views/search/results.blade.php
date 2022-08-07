159e681ac415218abc1341446c6f88ec2126ef0b
kfukumoto09 <kfukumoto@protein.osaka-u.ac.jp>
2022-08-07 22:16:50 +0900

    爆速でLaravel Sail × Reactアプリをherokuにデプロイしていく

--- resources/views/search/results.blade.php
+++ resources/views/search/results.blade.php
@@ -2,7 +2,7 @@
     
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Search lab notes
+            Search results
         </h2>
     </x-slot>
     
